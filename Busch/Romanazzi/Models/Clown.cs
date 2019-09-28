75e07736164826eb20db1fca7c1dce4671910431
marco752 <marco752slayer@gmail.com>
2019-09-28 12:32:32 -0400

    SegundoCommit

---
+++ Busch/Romanazzi/Models/Clown.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Romanazzi.Models
+{
+    public enum TypeList { Vela, Venado, Vaso}
+    public class Clown
+    {
+        [Key]
+        public int ClownID { get; set; }
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [MinLength(2, ErrorMessage = "Nombre muy corto")]
+        [MaxLength(24, ErrorMessage = "Nombre muy largo")]
+        public string NickName { get; set; }
+        [Required]
+        public TypeList Tricks { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Cumpleaños")]
+        public DateTime Birthday { get; set; }
+    }
+}
